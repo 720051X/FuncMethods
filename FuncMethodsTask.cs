1475f17433e52687bf4b4886e2d33e2149fe7af4
720051X <101687198+720051X@users.noreply.github.com>
2022-03-29 10:43:38 +1100

    task one

--- FuncMethodsTask.cs
+++ FuncMethodsTask.cs
@@ -1,1 +1,15 @@
-hello
+using System;
+
+public class Program
+{
+    static void SayHello() {
+        Console.WriteLine("Welcome Friends");
+        Console.WriteLine("Have A Nice Day");
+    }
+	public static void Main()
+	{
+        SayHello();
+    }
+}
+
+	
