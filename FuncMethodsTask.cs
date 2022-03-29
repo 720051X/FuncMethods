e84c2b0cf55c0e7763a230d2024bfb0b8df636d3
720051X <101687198+720051X@users.noreply.github.com>
2022-03-29 11:39:39 +1100

    taskfour

--- FuncMethodsTask.cs
+++ FuncMethodsTask.cs
@@ -2,13 +2,37 @@ using System;
 
 public class Program
 {
-    static void SayHello() {
-        Console.WriteLine("Welcome Friends");
-        Console.WriteLine("Have A Nice Day");
+                        // TASK ONE
+                        /* static void SayHello() {
+                            Console.WriteLine("Welcome Friends");
+                            Console.WriteLine("Have A Nice Day");
+                        } */
+
+                        // TASK TWO
+                        /*static void Welcome(string pName) {
+                            Console.WriteLine("Hello Friend " + pName);
+                            Console.WriteLine("Have A Nice Day");
+                        } */
+    
+                        // TASK THREE
+                        /* static void AddTwoNums(int n1, int n2) {
+                            Console.WriteLine("The sum of two numbers is: " +(n1 + n2));
+                        } */
+
+    static void SpaceCounter() {
+        Console.WriteLine("Please enter a string:");
+        string input = Console.ReadLine();
+        char space = ' ';
+        int spaceNumber = input.Split(space).Length - 1;
+        Console.WriteLine("\"" + input + "\" contains " + spaceNumber + " spaces.");
+
     }
 	public static void Main()
 	{
-        SayHello();
+       // SayHello();
+       // Welcome("John");
+       // AddTwoNums(20, 30);
+    SpaceCounter();
     }
 }
 
